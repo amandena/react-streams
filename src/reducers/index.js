0594cdcd249cb79bd3de7423a0a64a41db98ce63
amandena <manda.m.johnson@gmail.com>
2022-06-14 00:11:47 -0400

    imported auth reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
+import { authReducer } from './authReducer'
 
 export default combineReducers({
-  replaceMe: () => 'replace with reducers'
+  auth: authReducer
 })
