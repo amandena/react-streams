922f9431dc4fcf572c435ca925ad7947aee3551d
amandena <manda.m.johnson@gmail.com>
2022-07-05 20:53:57 -0400

    added remaining action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -31,3 +31,18 @@ export const fetchStreams = () => async dispatch => {
   const res = await streams.get('/streams')
   dispatch({ type: FETCH_STREAMS, payload: res.data })
 }
+
+export const fetchStream = id => async dispatch => {
+  const res = await streams.get(`/streams/${id}`)
+  dispatch({ type: FETCH_STREAM, payload: res.data })
+}
+
+export const editStream = (id, formValues) => async dispatch => {
+  const res = streams.put(`/streams/${id}`, formValues)
+  dispatch({ type: EDIT_STREAM, payload: res.data })
+}
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`)
+  dispatch({ type: DELETE_STREAM, payload: id })
+}
