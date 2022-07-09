c79ba9204aee352c5cb820819c7568cd283d2857
amandena <manda.m.johnson@gmail.com>
2022-07-08 20:16:08 -0400

    created history object with createBrowserHistory and set it up in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Router, Route } from 'react-router-dom'
 
 import Header from './Header'
 import StreamCreate from './streams/StreamCreate'
@@ -6,11 +6,12 @@ import StreamDelete from './streams/StreamDelete'
 import StreamEdit from './streams/StreamEdit'
 import StreamList from './streams/StreamList'
 import StreamShow from './streams/StreamShow'
+import history from '../history'
 
 const App = () => {
   return (
     <div className='ui containter'>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path='/' exact component={StreamList} />
@@ -19,7 +20,7 @@ const App = () => {
           <Route path='/streams/delete' exact component={StreamDelete} />
           <Route path='/streams/show' exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   )
 }
