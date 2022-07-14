affb95c04a2e16fb2ff2fc2186185eb58e52182e
amandena <manda.m.johnson@gmail.com>
2022-07-13 22:19:21 -0400

    refactored edit component to use stream form and added lodash & editStream imports

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import _ from 'lodash'
 import { connect } from 'react-redux'
 
-import { fetchStream } from '../../actions'
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './StreamForm'
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id)
+  }
+
+  onSubmit = (formValues) => {
+    console.log(formValues)
   }
 
   render() {
@@ -15,7 +21,11 @@ class StreamEdit extends React.Component {
 
     return (
       <div>
-        {this.props.stream.title}
+        <h3>Edit a Stream</h3>
+        <StreamForm 
+          onSubmit={this.onSubmit} 
+          initialValues={_.pick(this.props.stream, 'title', 'description')} 
+        />
       </div>
     )
   }
@@ -27,4 +37,4 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit)
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit)
