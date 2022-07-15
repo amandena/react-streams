d889763c4cb66fc9b66b62f5ddc834f2b91118f2
amandena <manda.m.johnson@gmail.com>
2022-07-15 13:27:09 -0400

    converted delete button into a link

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -13,9 +13,9 @@ class StreamList extends React.Component {
       return (
         <div className='right floated content'>
           <Link className='ui button primary' to={`/streams/edit/${stream.id}`}>Edit</Link>
-          <button className='ui button negative'>
+          <Link to={`/streams/delete/${stream.id}`} className='ui button negative'>
             Delete
-          </button>
+          </Link>
         </div>
       )
     }
